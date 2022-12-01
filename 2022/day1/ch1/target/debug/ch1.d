cd54931dde37cef35bac93c047ecdf4678fb3da0
Hannes Ziereis <hannes.ziereis@gmail.com>
2022-12-01 07:00:41 +0100

    Revert "init and day1"
    This reverts commit 580408203c4470e194cc599149c603b571f319ad.

--- 2022/day1/ch1/target/debug/ch1.d
+++
@@ -1,1 +0,0 @@
-/home/zierh/projects/adventofcode/2022/day1/ch1/target/debug/ch1: /home/zierh/projects/adventofcode/2022/day1/ch1/src/main.rs
--- 2022/day1/ch1/target/debug/deps/ch1-0eae2ffc6c419ca0.d
+++
@@ -1,5 +0,0 @@
-/home/zierh/projects/adventofcode/2022/day1/ch1/target/debug/deps/ch1-0eae2ffc6c419ca0: src/main.rs
-
-/home/zierh/projects/adventofcode/2022/day1/ch1/target/debug/deps/ch1-0eae2ffc6c419ca0.d: src/main.rs
-
-src/main.rs:
