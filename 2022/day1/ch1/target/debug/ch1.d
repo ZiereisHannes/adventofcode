580408203c4470e194cc599149c603b571f319ad
Hannes Ziereis <hannes.ziereis@gmail.com>
2022-12-01 06:58:15 +0100

    init and day1

---
+++ 2022/day1/ch1/target/debug/ch1.d
@@ -0,0 +1,1 @@
+/home/zierh/projects/adventofcode/2022/day1/ch1/target/debug/ch1: /home/zierh/projects/adventofcode/2022/day1/ch1/src/main.rs
---
+++ 2022/day1/ch1/target/debug/deps/ch1-7918584453e79f1e.d
@@ -0,0 +1,7 @@
+/home/zierh/projects/adventofcode/2022/day1/ch1/target/debug/deps/ch1-7918584453e79f1e.rmeta: src/main.rs
+
+/home/zierh/projects/adventofcode/2022/day1/ch1/target/debug/deps/ch1-7918584453e79f1e.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CLIPPY_ARGS=
